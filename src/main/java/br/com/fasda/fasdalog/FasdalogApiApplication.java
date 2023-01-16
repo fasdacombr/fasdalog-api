ef7d5c813d5c61f96365b2fb1d006113d15e9b56
Flavio <flavio_125@hotmail.com>
2023-01-16 17:00:14 -0300

    Criado o projeto inicial...

---
+++ src/main/java/br/com/fasda/fasdalog/FasdalogApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fasda.fasdalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasdalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasdalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fasda/fasdalog/FasdalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fasda.fasdalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasdalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
