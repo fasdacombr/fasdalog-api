2511c6e3a917496ca50c008274b9155495312ed0
Flavio <flavio_125@hotmail.com>
2023-01-19 16:56:48 -0300

    Usando Jakarta Persistence(JPA)

--- src/main/java/br/com/fasda/fasdalog/api/controller/ClienteController.java
+++ src/main/java/br/com/fasda/fasdalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.fasda.fasdalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,14 @@ import br.com.fasda.fasdalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Flávio");
-		cliente1.setTelefone("81 984633488");
-		cliente1.setEmail("flavio@fasda.com.br");
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("81 988888888");
-		cliente2.setEmail("maria@fasda.com.br");
-		
-		return Arrays.asList(cliente1, cliente2);
 	}
 
 }
