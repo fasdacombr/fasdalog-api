075c4563cf5d0aa4f469c8e7bcaac3f9e958e60e
Flavio <flavio_125@hotmail.com>
2023-01-19 20:19:00 -0300

    Usando Spring Data JPA

--- src/main/java/br/com/fasda/fasdalog/api/controller/ClienteController.java
+++ src/main/java/br/com/fasda/fasdalog/api/controller/ClienteController.java
@@ -2,24 +2,22 @@ package br.com.fasda.fasdalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fasda.fasdalog.domain.model.Cliente;
+import br.com.fasda.fasdalog.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findAll();
 		
 	}
 
---
+++ src/main/java/br/com/fasda/fasdalog/domain/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package br.com.fasda.fasdalog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.fasda.fasdalog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+
+}
